f4c61c2f5a1d327d9f063aef7bb7b377c0229ee3
Sammy2022 <86789353+Sammy2022@users.noreply.github.com>
2021-08-06 15:12:51 +0530

    New Code

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Power
+//
+//  Created by samarth kumar on 06/08/21.
+//
+/* To raise a number m by n and takind deafult value as 2 if value of n is not entered */
+
+#include<iostream>
+using namespace std;
+
+void power(double base, int exp = 2 )
+{
+    double ans = 1;
+    for(int i=1;i<=exp;i++)
+    {ans=ans*base;}
+    cout<<ans;
+}
+
+int main()
+
+{   int n;
+    double m;
+    char ch;
+    
+    cout<<"Enter Base Value"<<endl;
+    cin>>m;
+    
+    cout<<" Do you want to input Exponent Value (Y/N)?"<<endl;
+    cin>>ch;
+    
+    if((ch=='y')||(ch=='Y'))
+    { cout<<" Enter Exponent Value"<<endl;
+      cin>>n;
+      power(m,n); }
+    else{
+        power(m);
+    }
+    return 0;
+}
